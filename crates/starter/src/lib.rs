abc777bfe7e419db55953fba03649ca54a804fcb
Daniel Robson <danr.236@gmail.com>
2024-06-27 01:16:08 +0000

    fix missing docs

--- crates/starter/src/lib.rs
+++ crates/starter/src/lib.rs
@@ -1,3 +1,7 @@
+//! # Starter Rust Project
+//!
+//! This is a starter Rust project that contains the .github/workflows and various
+//! configuration files that I find useful for my Rust projects.
 pub fn hello() {
     println!("Hello, world!");
 }
