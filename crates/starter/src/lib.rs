7726226002c624dc6e49ee1f709b19e60d8d6229
Daniel Robson <danr.236@gmail.com>
2024-06-27 01:22:15 +0000

    spellcheck?

--- crates/starter/src/lib.rs
+++ crates/starter/src/lib.rs
@@ -2,6 +2,16 @@
 //!
 //! This is a starter Rust project that contains the .github/workflows and various
 //! configuration files that I find useful for my Rust projects.
+
+/// Print "Hello, world!" to the console.
+///
+/// # Examples
+///
+/// ```
+/// use starter::hello;
+///
+/// hello();
+/// ```
 pub fn hello() {
     println!("Hello, world!");
 }
--- examples/starter.rs
+++ examples/starter.rs
@@ -1,3 +1,5 @@
+//! This is a simple example of a Rust program that uses the `starter` library.
+
 use starter::hello;
 
 fn main() {
